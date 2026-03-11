dfa5fead50b42ebf007d812e9c50b0d9b1b4a4ef
Sergio Mejia <sergio12mg25@gmail.com>
2026-03-11 10:02:17 -0500

    Excepciones personalizadas

---
+++ src/main/domain/exceptions.py
@@ -0,0 +1,14 @@
+class User_NotFound_Error(Exception):
+    pass
+
+class User_EmailRepeated_Error(Exception):
+    pass
+
+class AccessDenied_Error(Exception):
+    pass
+
+class Course_NotFound_Error(Exception):
+    pass
+
+class Course_TitleRepeated_Error(Exception):
+    pass
