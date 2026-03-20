0b905ea3c71af52a0cd62db62a619cac40690924
Sergio Mejia <sergio12mg25@gmail.com>
2026-03-20 13:32:19 -0500

    Esquemas de Usuario para las solicitudes

---
+++ src/main/infraestructure/adapters/input/schemas/user.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel, EmailStr, Field
+
+# =========== BASIC SCHEMAS ===========
+# User registration
+class UserCreate(BaseModel):
+    name: str = Field(..., min_length=3, max_length=255)
+    email: EmailStr
+    password: str = Field(..., min_length=8, max_length=50)
+
+    class Config:
+        extra = "forbid"
+
+# User update
+class UserUpdate(BaseModel):
+    name: str | None = None
+    email: EmailStr | None = None
+    password: str | None = None
+
+    class Config:
+        extra = "forbid"
+
+# Response
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
+
+# =========== AUTH SCHEMAS ===========
+# Log-in
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=8, max_length=8)
+
+# Token for response
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    user: dict | None = None # Basic user data
+
+class TokenData(BaseModel):
+    user_id: int | None = None
+    email: str | None = None
