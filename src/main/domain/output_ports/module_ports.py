daba0ea9eb233a5ec8ccb74fe18573c7ec600943
Sergio Mejia <sergio12mg25@gmail.com>
2026-03-20 13:18:18 -0500

    Puertos de salida para el repositorio de módulos

---
+++ src/main/domain/output_ports/module_ports.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from src.main.domain.models.course import CModule, Topic
+
+class ModuleRepository(ABC):
+    @abstractmethod
+    def save(self, module: CModule) -> CModule:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, module_id: int) -> CModule | None:
+        pass
+
+    @abstractmethod
+    def find_by_title(self, title: str) -> CModule | None:
+        pass
+
+    @abstractmethod
+    def find_submodules(self, parent_id: int) -> List['CModule'] | None:
+        pass
+
+    @abstractmethod
+    def find_topics(self, module_id: int) -> Topic | None:
+        pass
+
+    @abstractmethod
+    def update(self, module: CModule) -> CModule | None:
+        pass
+
+    @abstractmethod
+    def delete_id(self, module_id: int) -> None:
+        pass
