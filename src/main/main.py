bdc75ecea7ba784545b1cbfebcac826237cd82b9
Sergio Mejia <sergio12mg25@gmail.com>
2026-07-03 13:44:09 -0500

    Se añaden los enrutadores para gestionar módulos y temas

--- src/main/main.py
+++ src/main/main.py
@@ -4,19 +4,24 @@ import uvicorn
 from src.main.utils.middlewares import ErrorMiddlewareAPI
 from src.main.infraestructure.adapters.input.routers.user_router import router as users_router
 from src.main.infraestructure.adapters.input.routers.course_router import router as courses_router
+from src.main.infraestructure.adapters.input.routers.module_router import router as modules_router
+from src.main.infraestructure.adapters.input.routers.topic_router import router as topics_router
 from src.main.infraestructure.adapters.input.routers.auth_router import router as auth_router
 
-app = FastAPI(title="API de E-Learning", version="1.0.0")
+# API Basic info
+app = FastAPI(title="E-Learning API", version="1.0.0")
 
 app.add_middleware(ErrorMiddlewareAPI)
 
-app.include_router(users_router)
 app.include_router(auth_router)
+app.include_router(users_router)
 app.include_router(courses_router)
+app.include_router(modules_router)
+app.include_router(topics_router)
 
 @app.get("/")
-def inicio():
-    return {"mensaje": "Hola mundo"}
+def home():
+    return {"message": "The API works!"}
 
 # Execution
 if __name__ == "__main__":
